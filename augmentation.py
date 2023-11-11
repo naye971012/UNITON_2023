03f087d4b10c541d1a6159ef82035d2e200969f2
naye971012 <naye03@naver.com>
2023-11-11 22:09:50 +0900

    add mixed

--- augmentation.py
+++ augmentation.py
@@ -13,7 +13,8 @@ class transformation_class:
         "test_transform" : self.test_transform,
         "base_transform" : self.base_transform,
         "hard_transform" : self.hard_transform,
-        "mask_transform" : self.mask_transform
+        "mask_transform" : self.mask_transform,
+        "mixed_transform" : self.mixed_transform
         }
             
     def __call__(self, name:str):
@@ -71,6 +72,35 @@ class transformation_class:
                 A.Rotate(limit=10, p=0.5),                   # -10도에서 10도 사이 랜덤 회전
             ], p=0.75),  # OneOf로 랜덤 선택, p=1.0은 항상 적용하도록 함
             
+            A.GridDropout(holes_number_x=6, holes_number_y=6, p=0.75),
+
+            A.OneOf([
+                A.RandomCrop(height=334, width=334, p=0.7),  # 무작위 크롭 (정사각형으로 crop)
+                A.RandomCrop(height=384, width=384, p=0.8),  # 무작위 크롭 (정사각형으로 crop)
+                A.RandomCrop(height=448, width=448, p=0.9),  # 무작위 크롭 (정사각형으로 crop)
+                A.RandomCrop(height=496, width=496, p=1),  # 무작위 크롭 (정사각형으로 crop)
+            ], p=0.3),  # OneOf로 랜덤 선택, p=1.0은 항상 적용하도록 함
+            
+            
+            A.Resize(*self.resize),
+            A.Normalize(),
+            ToTensorV2()
+        ])
+        
+        
+        self.mixed_transform = A.Compose([
+
+            A.OneOf([
+                A.HorizontalFlip(p=0.5),  # 수평 뒤집기
+                A.Rotate(limit=10, p=0.5),                   # -10도에서 10도 사이 랜덤 회전
+            ], p=0.75),  # OneOf로 랜덤 선택, p=1.0은 항상 적용하도록 함
+            
+            A.RandomBrightnessContrast(p=0.15),  # 랜덤한 밝기와 대비 조절
+            A.RandomBrightness(p=0.15),                       # 랜덤 밝기 조절
+            A.HueSaturationValue(p=0.15),                    # 색조, 채도, 명도 조절
+            A.RandomGamma(p=0.15),                           # 랜덤 감마 조절
+            A.GaussNoise(var_limit=(0.0, 10.0), p=0.15),     # 가우시안 노이즈 추가
+
             A.GridDropout(holes_number_x=6, holes_number_y=6, p=0.75),
 
             A.OneOf([
